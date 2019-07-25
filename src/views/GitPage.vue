2edbd8214e31b4573f8f5e6deabb7bfac977bc3b
KimHoon <gnsl8391@gmail.com>
2019-07-25 17:53:07 +0900

    S1P3113002-47 | 190725 | merge algorithm 수정

--- src/views/GitPage.vue
+++ src/views/GitPage.vue
@@ -39,9 +39,9 @@ export default {
       this.commits.push(event_1);
       const response2 = GitlabService.getMerges(projectID);
       this.merges = response2;
-      console.log(this.merges);
+      // console.log(this.commits);
     },
-    init() {
+    async init() {
       let branches = [];
       const graphContainer = document.getElementById("gitGraph");
       const gitgraph = createGitgraph(graphContainer, {
@@ -64,51 +64,75 @@ export default {
         author: "tastera <SONG KUNHEE>"
       });
       const commits = this.commits;
+      const merges = this.merges;
       for (let k = 0; k < commits.length; k++) {
-        commits[k].then(response => {
+        await commits[k].then(async function(response) {
           for (let i = response.data.length - 1; i > -1; i--) {
-            console.log(response.data[i]);
+            // console.log(response.data[i]);
             if (
               response.data[i].action_name == "pushed new" &&
               response.data[i].push_data.ref != "master" &&
               response.data[i].push_data.ref != "developers" &&
               response.data[i].push_data.ref != "develop"
             ) {
+              // console.log(response.data[i]);
               var exbranch = gitgraph.branch(response.data[i].push_data.ref);
               exbranch.checkout();
               branches.push(exbranch);
+              // console.log(branches);
             } else if (response.data[i].action_name == "pushed to") {
-              branches.forEach(e_branch => {
+              // console.log(response.data[i]);
+              branches.forEach(function(e_branch) {
                 if (e_branch.name == response.data[i].push_data.ref) {
-                  console.log(e_branch);
                   e_branch.commit({
                     subject: response.data[i].push_data.commit_title,
                     author: `${response.data[i].author.username} <${
                       response.data[i].author.name
-                    }>`
+                    }>`,
+                    onClick() {
+                      window.open(
+                        `https://lab.ssafy.com/tastera/webmobile-sub2/commit/${
+                          response.data[i].push_data.commit_to
+                        }`
+                      );
+                    },
+                    onMessageClick() {
+                      window.open(
+                        `https://lab.ssafy.com/tastera/webmobile-sub2/commit/${
+                          response.data[i].push_data.commit_to
+                        }`
+                      );
+                    }
                   });
                   e_branch.checkout();
                   return false;
                 }
               });
             } else if (response.data[i].action_name == "accepted") {
-              const merges = this.merges;
-              branches.forEach(e_branch => {
-                merges.then(e_merge => {
+              // console.log(response.data[i]);
+              await branches.forEach(async function(e_branch, i_branch) {
+                await merges.then(function(e_merge) {
                   for (let j = 0; j < e_merge.data.length; j++) {
                     if (
                       response.data[i].target_title == e_merge.data[j].title
                     ) {
                       if (e_branch.name == e_merge.data[j].source_branch) {
+                        // console.log(branches);
+                        // console.log(e_branch.name);
+                        // console.log(e_merge.data[j].source_branch);
                         develop.merge({
                           branch: e_branch.name,
                           commitOptions: {
-                            subject: response.data[i].target_title,
+                            subject: `MergeRequest / ${
+                              response.data[i].target_title
+                            }`,
                             author: `${response.data[i].author.username} <${
                               response.data[i].author.name
                             }>`
                           }
                         });
+                        branches.splice(branches.indexOf(i_branch), 1);
+                        // console.log(branches);
                       }
                     }
                   }
@@ -117,87 +141,15 @@ export default {
             }
           }
         });
-      //   commits[k].then(r => {
-      //     for (let i = r.data.length - 1; i >= 0; i--) {
-      //       if (r.data[i].action_name == "pushed new") {
-      //         var b = gitgraph.branch(r.data[i].push_data.ref);
-      //         b.commit({
-      //           subject: r.data[i].push_data.commit_title,
-      //           author: r.data[i].author.username,
-      //           onClick() {
-      //             window.open(
-      //               `https://lab.ssafy.com/tastera/webmobile-sub2/commit/${
-      //                 r.data[i].push_data.commit_to
-      //               }`
-      //             );
-      //           },
-      //           onMessageClick() {
-      //             window.open(
-      //               `https://lab.ssafy.com/tastera/webmobile-sub2/commit/${
-      //                 r.data[i].push_data.commit_to
-      //               }`
-      //             );
-      //           }
-      //         });
-      //         b.checkout();
-      //         branches.push(b);
-      //       } else if (r.data[i].action_name == "accepted") {
-      //         branches.forEach(e => {
-      //           const merges = this.merges;
-      //           merges.then(m => {
-      //             for (let j = 0; j < m.data.length; j++) {
-      //               if (r.data[i].target_title == m.data[j].title) {
-      //                 if (e.name == m.data[j].source_branch) {
-      //                   master.merge({
-      //                     branch: e,
-      //                     commitOptions: {
-      //                       subject: r.data[i].target_title,
-      //                       author: r.data[i].author.username
-      //                     },
-      //                     onClick() {
-      //                       window.open(`https://lab.ssafy.com/tastera/webmobile-sub2/commit/
-      //                             ${r.data[i].push_data.commit_to}`);
-      //                     },
-      //                     onMessageClick() {
-      //                       window.open(`https://lab.ssafy.com/tastera/webmobile-sub2/commit/
-      //                             ${r.data[i].push_data.commit_to}`);
-      //                     }
-      //                   });
-      //                   return false;
-      //                 }
-      //                 return false;
-      //               }
-      //             }
-      //           });
-      //         });
-      //       } else if (r.data[i].action_name == "pushed to") {
-      //         branches.forEach(e => {
-      //           if (e.name == r.data[i].push_data.ref) {
-      //             var title = r.data[i].push_data.commit_title;
-      //             if (title.substr(0, 5) != "Merge") {
-      //               e.commit({
-      //                 subject: r.data[i].push_data.commit_title,
-      //                 author: r.data[i].author.username,
-      //                 onClick() {
-      //                   window.open(`https://lab.ssafy.com/tastera/webmobile-sub2/commit/
-      //                       ${r.data[i].push_data.commit_to}`);
-      //                 },
-      //                 onMessageClick() {
-      //                   window.open(`https://lab.ssafy.com/tastera/webmobile-sub2/commit/
-      //                       ${r.data[i].push_data.commit_to}`);
-      //                 }
-      //               });
-      //               e.checkout();
-      //               return false;
-      //             }
-      //             return false;
-      //           }
-      //         });
-      //       }
-      //     }
-      //   });
       }
-      console.log(branches)
+      // console.log(branches);
+      master.merge({
+        branch: develop,
+        commitOptions: {
+          subject: "final develop => master merge",
+          author: "gnsl8391 <KimHoon>"
+        }
+      });
     }
   }
 };
@@ -221,6 +173,6 @@ path {
 svg {
   transform: scale(0.5);
   margin-left: -380px;
-  margin-top: -750px;
+  margin-top: -1050px;
 }
 </style>
