d27f1f3e2a35555bd78ac8a2c37052847bc33b60
yeong <yex38144@naver.com>
2019-08-02 14:56:47 +0900

    error clean

--- sql/comment.sql
+++ sql/comment.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS pfComment;
 
 CREATE TABLE pfComment(
 	pfc_id INT auto_increment primary key,
-    pfid varchar(100) not null,
+    pfid varchar(100) not null, # pfid는 portfolio id
     pfc_writer varchar(100) not null,
     pfc_writerUid varchar(100) not null,
     pfc_content varchar(500) not null,
@@ -24,6 +24,27 @@ CREATE TABLE pfSubComment(
     foreign key (pfc_id) references pfComment(pfc_id) ON DELETE CASCADE
 );
 
+CREATE TABLE postComment(
+	pc_id INT auto_increment primary key,
+    pid varchar(100) not null,   # pid는 post id
+    pc_writer varchar(100) not null,
+    pc_writerUid varchar(100) not null,
+    pc_content varchar(500) not null,
+    pc_date DATETIME DEFAULT now()
+);
+
+
+CREATE TABLE postSubComment(
+	spc_id INT auto_increment primary key,
+    pid varchar(100) not null,
+    spc_writer varchar(100) not null,
+    spc_writerUid varchar(100) not null,
+    spc_content varchar(500) not null,
+    spc_date DATETIME DEFAULT now(),
+    pc_id int,
+    foreign key (pc_id) references postComment(pc_id) ON DELETE CASCADE
+);
+
 insert into pfComment values(0,"becmvtUI87RiknyZApQ2","정총무","ipFznAij4vWZBGwpDuJVXldCCXv1","테스트 완투쓰리",default);
 insert into pfComment values(0,"becmvtUI87RiknyZApQ2","정총무","ipFznAij4vWZBGwpDuJVXldCCXv1","테스트 쓰리투완",default);
 
@@ -31,6 +52,8 @@ select * from pfComment where pfid = "becmvtUI87RiknyZApQ2";
 delete from pfComment where pfc_id = 3;
 
 insert into pfSubComment values(0,"becmvtUI87RiknyZApQ2","정총무","ipFznAij4vWZBGwpDuJVXldCCXv1","테스트 완투쓰리",default,1);
-insert into pfSubComment values(0,"becmvtUI87RiknyZApQ2","정총무","ipFznAij4vWZBGwpDuJVXldCCXv1","피자",default,1);
+insert into pfSubComment values(0,"becmvtUI87RiknyZApQ2","정총무","ipFznAij4vWZBGwpDuJVXldCCXv1","테스트",default,cast('1' as unsigned));
 select * from pfSubComment where pfc_id = 1;
 delete from pfSubComment where spfc_id = 3;
+
+select * from postComment;
