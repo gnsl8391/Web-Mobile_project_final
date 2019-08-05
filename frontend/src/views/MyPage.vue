7f5ab5c96d7e3b5b9024d83ff7a46db94565f5fd
yeong <yex38144@naver.com>
2019-08-05 16:51:18 +0900

    S1P3113002-54 | mypage 포스트, 포트폴리오 정보 로딩

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -73,6 +73,51 @@
               </v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
+          <v-divider inset></v-divider>
+          <!-- 포트폴리오 -->
+          <v-list-tile class="listTile">
+            <v-list-tile-action>
+              <v-icon class="fas fa-file-powerpoint" size="30px"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>My Portfolios</v-list-tile-title>
+              <v-list-tile-sub-title>
+                나의 포트폴리오 개수 : {{ pfCount   }}개
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile class="listTile">
+            <v-list-tile-action />
+            <v-list-tile-content>
+              <v-list-tile-title>최근 게시한 포트폴리오</v-list-tile-title>
+              <v-list-tile-sub-title>
+                <a @click="getPfDetail()">{{ pfTitle }}</a>
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider inset></v-divider>
+          <!-- 포스트 -->
+          <v-list-tile class="listTile">
+            <v-list-tile-action>
+              <v-icon class="fas fa-file-alt" size="30px"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>My Posts</v-list-tile-title>
+              <v-list-tile-sub-title>
+                나의 포스트 개수 : {{ptCount}}개
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile class="listTile">
+            <v-list-tile-action />
+            <v-list-tile-content>
+              <v-list-tile-title>최근 게시한 포스트</v-list-tile-title>
+              <v-list-tile-sub-title>
+                  <a @click="getPtDetail()">{{ ptTitle }}</a>
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider inset></v-divider>
           <div style="margin:15px 0;float:right">
             <v-btn flat
              color="warning"
@@ -119,11 +164,19 @@ export default{
       x: null,
       mode: "",
       timeout: 6000,
-      text: ""
+      text: "",
+      pfCount: 0,
+      ptCount: 0,
+      pfInfo: {},
+      ptInfo: {},
+      pfTitle: "",
+      ptTitle: ""
     };
   },
   created() {
     this.curAuthChk();
+    this.getPort();
+    this.getPost();
   },
   computed: {
     chkMyauth() {
@@ -155,18 +208,95 @@ export default{
         this.myauth = auth.myauth;
       });
     },
+    getPort() {
+      var myPf = FirebaseService.getOnePortfolio(this.$store.state.user.uid);
+      myPf.then(data => {
+        var count = 0;
+        var res = true;
+        for (var i = 0; i < data.length; i++) {
+          if (data[i] != null) {
+            if (res) {
+              this.pfInfo = data[i];
+              this.pfTitle = this.pfInfo.dataMap.title;
+              res = false;
+            }
+            count++;
+          }
+        }
+        this.pfCount = count;
+        if (res) {
+          this.pfTitle = "게시한 포트폴리오가 없습니다.";
+        }
+      });
+    },
+    getPost() {
+      var myPt = FirebaseService.getOnePost(this.$store.state.user.uid);
+      myPt.then(data => {
+        console.log(data);
+        var count = 0;
+        var res = true;
+        for (var i = 0; i < data.length; i++) {
+          if (data[i] != null) {
+            if (res) {
+              console.log(data[i]);
+              this.ptInfo = data[i];
+              this.ptTitle = this.ptInfo.dataMap.title;
+              res = false;
+            }
+            count++;
+          }
+        }
+        this.ptCount = count;
+        if (res) {
+          this.ptTitle = "게시한 포스트가 없습니다.";
+        }
+      });
+    },
     updateMember() {
+      FirebaseService.updatePassword(this.password);
       this.text = "수정되었습니다. 다시 로그인해주세요.";
       this.snackbar = true;
     },
     deleteMember() {
-      this.text = "안녕히 가십시오.";
-      this.snackbar = true;
+      var con = confirm("탈퇴하시겠습니까?");
+      if (con) {
+        FirebaseService.deleteAccount();
+        this.text = "안녕히 가십시오.";
+        this.snackbar = true;
+      }
     },
     goHome() {
       this.snackbar = false;
       FirebaseService.logout();
       this.$router.push("/");
+    },
+    getPfDetail() {
+      if (this.pfCount != 0) {
+        this.$router.push({name: "portfolioDetail",
+          params: {
+            pfid: this.pfInfo.id,
+            date: this.pfInfo.dataMap.created_at,
+            title: this.pfInfo.dataMap.title,
+            body: this.pfInfo.dataMap.body,
+            imgSrc: this.pfInfo.dataMap.img,
+            uid: this.pfInfo.dataMap.uid,
+            writer: this.pfInfo.dataMap.writer
+          }});
+      }
+    },
+    getPtDetail() {
+      if (this.ptCount != 0) {
+        this.$router.push({name: "postDetail",
+          params: {
+            pid: this.ptInfo.id,
+            date: this.ptInfo.dataMap.created_at,
+            title: this.ptInfo.dataMap.title,
+            body: this.ptInfo.dataMap.body,
+            uid: this.ptInfo.dataMap.uid,
+            writer: this.ptInfo.dataMap.writer
+          }
+        });
+      }
     }
   }
 };
