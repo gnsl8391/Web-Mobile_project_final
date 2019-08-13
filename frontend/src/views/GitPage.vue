fc6e903f9e801d0c133a31c4823c6627c3dc5a90
KimHoon <gnsl8391@gmail.com>
2019-08-13 17:22:14 +0900

    err 수정

--- frontend/src/views/GitPage.vue
+++ frontend/src/views/GitPage.vue
@@ -129,7 +129,6 @@ export default {
                     }
                   })
                   .catch(function(err) {
-                    console.log(err);
                   });
               });
             }
